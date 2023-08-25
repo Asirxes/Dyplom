e395a3251d9b9b1594124456bfbf79b76d597f2c
Asirxes <karolkowalczykpraca0@gmail.com>
2023-08-25 19:10:40 +0200

    Dodanie funkcjonalności związanych z kontem

--- lib/main.dart
+++ lib/main.dart
@@ -1,115 +1,234 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Studenci dla studentów',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.brown,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  bool _isLoggedIn = false;
+  String? _email;
+  String? _password;
+  SharedPreferences? _prefs;
 
-  void _incrementCounter() {
+  @override
+  void initState() {
+    super.initState();
+    _loadUserState();
+  }
+
+  _loadUserState() async {
+    _prefs = await SharedPreferences.getInstance();
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      _isLoggedIn = _prefs!.getBool('isLoggedIn') ?? false;
+      _email = _prefs!.getString('email');
+      _password = _prefs!.getString('password');
     });
+  }
+
+  _register(String email, String password) async {
+    if (_prefs!.getString(email) == null) {
+      if (password.length >= 8) {
+        _prefs!.setString(email, password);
+        _login(email, password);
+        Fluttertoast.showToast(msg: "Rejestracja udana!");
+      } else {
+        Fluttertoast.showToast(msg: "Hasło musi mieć co najmniej 8 znaków!");
+      }
+    } else {
+      Fluttertoast.showToast(msg: "Email jest już zajęty!");
+    }
+  }
+
+  _login(String email, String password) async {
+    String? storedPassword = _prefs!.getString(email);
+    if (storedPassword == password) {
+      _prefs!.setBool('isLoggedIn', true);
+      _prefs!.setString('email', email);
+      _prefs!.setString('password', password);
+      setState(() {
+        _isLoggedIn = true;
+        _email = email;
+        _password = password;
+      });
+      Fluttertoast.showToast(msg: "Zalogowano pomyślnie!");
+    } else {
+      Fluttertoast.showToast(msg: "Złe dane logowania!");
+    }
+  }
+
+  _logout() {
+    setState(() {
+      _isLoggedIn = false;
+    });
+    _prefs!.remove('isLoggedIn');
+    Fluttertoast.showToast(msg: "Wylogowano!");
+  }
+
+  _changePassword(String newPassword) {
+    if (newPassword.length >= 8) {
+      _prefs!.setString(_email!, newPassword);
+      Fluttertoast.showToast(msg: "Hasło zostało zmienione!");
+    } else {
+      Fluttertoast.showToast(msg: "Hasło musi mieć co najmniej 8 znaków!");
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text('Studenci dla studentów'),
+        backgroundColor: Colors.brown,
+        actions: [
+          if (_isLoggedIn)
+            ...[
+              IconButton(
+                icon: Icon(Icons.person),
+                onPressed: () {
+                  _showInfoDialog();
+                },
+              ),
+              IconButton(
+                icon: Icon(Icons.vpn_key),
+                onPressed: () {
+                  _showChangePasswordDialog();
+                },
+              ),
+              IconButton(
+                icon: Icon(Icons.logout),
+                onPressed: _logout,
+              ),
+            ]
+          else
+            ...[
+              IconButton(
+                icon: Icon(Icons.login),
+                onPressed: () {
+                  _showDialog('Logowanie', _login);
+                },
+              ),
+              IconButton(
+                icon: Icon(Icons.person_add),
+                onPressed: () {
+                  _showDialog('Rejestracja', _register);
+                },
+              ),
+            ],
+        ],
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+        child: Text(_isLoggedIn ? 'Zalogowano jako: $_email' : 'Nie jesteś zalogowany'),
+      ),
+    );
+  }
+
+  _showDialog(String title, Function(String, String) onActionPressed) {
+    final emailController = TextEditingController();
+    final passwordController = TextEditingController();
+
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: Text(title),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              TextField(
+                controller: emailController,
+                decoration: InputDecoration(labelText: 'Email'),
+              ),
+              TextField(
+                controller: passwordController,
+                decoration: InputDecoration(labelText: 'Hasło'),
+                obscureText: true,
+              ),
+            ],
+          ),
+          actions: [
+            TextButton(
+              onPressed: () => Navigator.pop(context),
+              child: Text('Anuluj'),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+            TextButton(
+              onPressed: () {
+                onActionPressed(emailController.text, passwordController.text);
+                Navigator.pop(context);
+              },
+              child: Text('Zatwierdź'),
             ),
           ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+        );
+      },
+    );
+  }
+
+  _showChangePasswordDialog() {
+    final passwordController = TextEditingController();
+
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: Text('Zmień hasło'),
+          content: TextField(
+            controller: passwordController,
+            decoration: InputDecoration(labelText: 'Nowe hasło'),
+            obscureText: true,
+          ),
+          actions: [
+            TextButton(
+              onPressed: () => Navigator.pop(context),
+              child: Text('Anuluj'),
+            ),
+            TextButton(
+              onPressed: () {
+                _changePassword(passwordController.text);
+                Navigator.pop(context);
+              },
+              child: Text('Zatwierdź'),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  _showInfoDialog() {
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: Text('Informacje o użytkowniku'),
+          content: Text('Email: $_email\nHasło: $_password'),
+          actions: [
+            TextButton(
+              onPressed: () => Navigator.pop(context),
+              child: Text('Zamknij'),
+            ),
+          ],
+        );
+      },
     );
   }
 }
