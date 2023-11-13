1dfc8a16d1196ca5ed9a4ad5fb1bfe15a96a9a7d
Monika4270 <monika42701@gmail.com>
2023-11-13 08:21:56 +0100

    m

--- lib/dostepnosc_screen/dostepnosc_screen.dart
+++ lib/dostepnosc_screen/dostepnosc_screen.dart
@@ -2,6 +2,7 @@ import 'package:dyplom/colors.dart';
 import 'package:dyplom/AppBar/AppBar1.dart';
 import 'package:dyplom/AppBar/BottomNavigationBar.dart';
 import 'package:dyplom/theme/theme.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
@@ -16,6 +17,7 @@ class _DostepnoscScreenState extends State<DostepnoscScreen> {
   @override
   Widget build(BuildContext context) {
     final themeNotifier = Provider.of<ThemeNotifier>(context);
+    User? user = FirebaseAuth.instance.currentUser;
 
     return Scaffold(
       appBar: AppBar10(),
@@ -23,58 +25,58 @@ class _DostepnoscScreenState extends State<DostepnoscScreen> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-              // Text(
-              //   'Current Theme',
-              //   style: TextStyle(fontSize: 20),
-              //   //style: Theme.of(context).textTheme.headline6,
-              // ),
             SizedBox(height: 20),
             ElevatedButton(
               onPressed: () {
                 themeNotifier.setTheme(AppTheme.Red);
               },
-              style: ElevatedButton.styleFrom(primary: red1,
-              onPrimary: Colors.white,
-              side: BorderSide(color: Colors.white,),
-              
+              style: ElevatedButton.styleFrom(
+                primary: red1,
+                onPrimary: Colors.white,
+                side: BorderSide(color: Colors.white),
               ),
               child: Text('A'),
             ),
+            SizedBox(height: 10),
             ElevatedButton(
               onPressed: () {
                 themeNotifier.setTheme(AppTheme.Yellow);
               },
-              style: ElevatedButton.styleFrom(primary: Colors.black,
-              onPrimary: yellow1,
-              side: BorderSide(color: yellow1,),
+              style: ElevatedButton.styleFrom(
+                primary: Colors.black,
+                onPrimary: yellow1,
+                side: BorderSide(color: yellow1),
               ),
               child: Text('A'),
             ),
+            SizedBox(height: 10),
             ElevatedButton(
               onPressed: () {
                 themeNotifier.setTheme(AppTheme.BlacknY);
               },
-              style: ElevatedButton.styleFrom(primary: yellow1,
-              onPrimary: Colors.black,
-              side: BorderSide(color: Colors.black,),
+              style: ElevatedButton.styleFrom(
+                primary: yellow1,
+                onPrimary: Colors.black,
+                side: BorderSide(color: Colors.black),
               ),
               child: Text('A'),
             ),
-
+            SizedBox(height: 10),
             ElevatedButton(
               onPressed: () {
                 themeNotifier.setTheme(AppTheme.Black);
               },
-              style: ElevatedButton.styleFrom(primary: Colors.black,
-              onPrimary: Colors.white,
-              side: BorderSide(color: Colors.white,),
-            ),
+              style: ElevatedButton.styleFrom(
+                primary: Colors.black,
+                onPrimary: Colors.white,
+                side: BorderSide(color: Colors.white),
+              ),
               child: Text('A'),
             ),
           ],
         ),
       ),
-      bottomNavigationBar: CustomBottomNavigationBar(),
+      bottomNavigationBar: user != null ? CustomBottomNavigationBar() : null,
     );
   }
 }
---
+++ lib/user_pages/logout_page.dart
@@ -0,0 +1,85 @@
+import 'package:dyplom/dostepnosc_screen/dostepnosc_screen.dart';
+import 'package:dyplom/user_pages/password_change_page.dart';
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+import '../glowny_screen/glowny_screen.dart';
+
+class LogoutPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Konto"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SizedBox(height: 40.0),
+            ElevatedButton(
+              onPressed: () {
+                _logout(context);
+              },
+              child: Text('Wyloguj się'),
+              style: ElevatedButton.styleFrom(
+                padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 80.0),
+                minimumSize: Size(200.0, 0), // Ustawienie minimalnej szerokości
+                textStyle: TextStyle(fontSize: 18.0),
+              ),
+            ),
+            SizedBox(height: 20.0),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => PasswordChangePage()),
+                );
+              },
+              child: Text('Edycja hasła'),
+              style: ElevatedButton.styleFrom(
+                padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 80.0),
+                minimumSize: Size(200.0, 0),
+                textStyle: TextStyle(fontSize: 18.0),
+              ),
+            ),
+            SizedBox(height: 20.0),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => DostepnoscScreen()),
+                );
+              },
+              child: Text('Dostępność'),
+              style: ElevatedButton.styleFrom(
+                padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 80.0),
+                minimumSize: Size(200.0, 0),
+                textStyle: TextStyle(fontSize: 18.0),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Future<void> _logout(BuildContext context) async {
+    FirebaseAuth _auth = FirebaseAuth.instance;
+    await _auth.signOut();
+    await checkLoginStatus(context);
+    Navigator.of(context).popUntil(ModalRoute.withName('/'));
+  }
+
+  Future<void> checkLoginStatus(BuildContext context) async {
+    FirebaseAuth auth = FirebaseAuth.instance;
+    User? user = auth.currentUser;
+
+    if (user == null) {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => EkranGlowny()),
+      );
+    }
+  }
+}
