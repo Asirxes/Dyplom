067326c7558830ac33a7bd2e02ddaf0fd5b85d37
Monika4270 <monika42701@gmail.com>
2023-11-13 09:36:37 +0100

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,36 +1,26 @@
 //import 'package:dyplom/tresc_screen/CategoryRepository.dart';
 import 'package:flutter/material.dart';
-import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:fluttertoast/fluttertoast.dart';
-
-import 'home_page.dart';
-import 'user_pages/login_page.dart';
-import 'user_pages/logout_page.dart';
-import 'user_pages/password_change_page.dart';
-import 'user_pages/registration_page.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:provider/provider.dart';
 import 'package:dyplom/theme/theme.dart';
 import 'package:dyplom/glowny_screen/glowny_screen.dart';
 
 import 'package:dyplom/ranking_screen/RatingModel.dart';
 import 'package:dyplom/tresc_screen/Category.dart';
-// //-----KAROLOWA BAZA-------------------------------------------------------------------------------------------
-void main() async {
+
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: FirebaseOptions(
-      apiKey: 'AIzaSyBNd_Vx1xdImlJ1sZg6WfTDfhYqic5A990',
-      authDomain: 'dyplom-986dd.firebaseapp.com',
-      projectId: 'dyplom-986dd',
-      storageBucket: 'dyplom-986dd.appspot.com',
-      messagingSenderId: '232096191273',
-      appId: '1:232096191273:web:051de7f6986206c56b5d38',
-      measurementId: 'G-HE5TEZBXRF',
+  await Firebase.initializeApp();
+  runApp(
+    ChangeNotifierProvider<CategoryRepository>(
+      create: (context) => CategoryRepository(),
+      child: MyApp(),
     ),
   );
-  runApp(MyApp());
 }
-//---------------------------------------------------------------------------------------------------
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
   @override
@@ -242,149 +232,182 @@ class CategoryRepository extends ChangeNotifier {
   }
 }
 
-class HomePage extends StatefulWidget {
-  @override
-  _HomePageState createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
-  // final FirebaseAuth _auth = FirebaseAuth.instance;
-  // User? _user;
-
-  // @override
-  // void initState() {
-  //   super.initState();
-  //   _user = _auth.currentUser;
-  // }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Studenci dla studentów"),
-        // actions: [
-        //   _user == null
-        //       ? IconButton(
-        //           icon: Icon(Icons.login),
-        //           onPressed: () => _showLoginDialog(context),
-        //         )
-        //       : IconButton(
-        //           icon: Icon(Icons.logout),
-        //           onPressed: () => _logout(),
-        //         ),
-        //   _user == null
-        //       ? IconButton(
-        //           icon: Icon(Icons.app_registration),
-        //           onPressed: () => _showRegistrationDialog(context),
-        //         )
-        //       : SizedBox.shrink(),
-        // ],
-      ),
-      // body: Center(
-      //   child:
-      //       _user == null ? Text('Zaloguj się') : Text('Witaj ${_user!.email}'),
-      // ),
-    );
-  }
-
-  void _showLoginDialog(BuildContext context) {
-    TextEditingController emailController = TextEditingController();
-    TextEditingController passwordController = TextEditingController();
-
-    showDialog(
-      context: context,
-      builder: (BuildContext context) {
-        return AlertDialog(
-          content: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              TextField(
-                controller: emailController,
-                decoration: InputDecoration(labelText: 'Email'),
-              ),
-              TextField(
-                controller: passwordController,
-                decoration: InputDecoration(labelText: 'Hasło'),
-                obscureText: true,
-              ),
-            ],
-          ),
-          // actions: [
-          //   TextButton(
-          //     onPressed: () {
-          //       _login(emailController.text, passwordController.text);
-          //       Navigator.pop(context);
-          //     },
-          //     child: Text('Zaloguj'),
-          //   )
-          // ],
-        );
-      },
-    );
-  }
-
-  // Future<void> _login(String email, String password) async {
-  //   try {
-  //     // UserCredential userCredential = await _auth.signInWithEmailAndPassword(
-  //     //   email: email,
-  //     //   password: password,
-  //     // );
-  //     // setState(() {
-  //     //   _user = userCredential.user;
-  //     // });
-  //     Fluttertoast.showToast(
-  //       msg: "Zalogowano",
-  //       toastLength: Toast.LENGTH_SHORT,
-  //       gravity: ToastGravity.BOTTOM,
-  //     );
-  //   } catch (e) {
-  //     Fluttertoast.showToast(
-  //       msg: "Nieprawidłowy email lub hasło",
-  //       toastLength: Toast.LENGTH_SHORT,
-  //       gravity: ToastGravity.BOTTOM,
-  //     );
-  //   }
-  // }
-
-  // void _showRegistrationDialog(BuildContext context) {
-  //   TextEditingController emailController = TextEditingController();
-  //   TextEditingController passwordController = TextEditingController();
-
-  //   showDialog(
-  //     context: context,
-  //     builder: (BuildContext context) {
-  //       return AlertDialog(
-  //         content: Column(
-  //           mainAxisSize: MainAxisSize.min,
-  //           children: [
-  //             TextField(
-  //               controller: emailController,
-  //               decoration: InputDecoration(labelText: 'Email'),
-  //             ),
-  //             TextField(
-  //               controller: passwordController,
-  //               decoration: InputDecoration(labelText: 'Hasło'),
-  //               obscureText: true,
-  //             ),
-  //           ],
-  //         ),
-  //         actions: [
-  //           TextButton(
-  //             onPressed: () {
-  //               _register(emailController.text, passwordController.text);
-  //               Navigator.pop(context);
-  //             },
-  //             child: Text('Zarejestruj'),
-  //           )
-  //         ],
-        // );
-      // },
-    // );
-  // }
-}
 
 
+//---------------------------------------------------------------------------------------------------
+// class HomePage extends StatefulWidget {
+//   @override
+//   _HomePageState createState() => _HomePageState();
+// }
 
+// class _HomePageState extends State<HomePage> {
+//   final FirebaseAuth _auth = FirebaseAuth.instance;
+//   User? _user;
 
+//   @override
+//   void initState() {
+//     super.initState();
+//     _user = _auth.currentUser;
+//   }
 
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text("Studenci dla studentów"),
+//         actions: [
+//           _user == null
+//               ? IconButton(
+//                   icon: Icon(Icons.login),
+//                   onPressed: () => _showLoginDialog(context),
+//                 )
+//               : IconButton(
+//                   icon: Icon(Icons.logout),
+//                   onPressed: () => _logout(),
+//                 ),
+//           _user == null
+//               ? IconButton(
+//                   icon: Icon(Icons.app_registration),
+//                   onPressed: () => _showRegistrationDialog(context),
+//                 )
+//               : SizedBox.shrink(),
+//         ],
+//       ),
+//       body: Center(
+//         child:
+//             _user == null ? Text('Zaloguj się') : Text('Witaj ${_user!.email}'),
+//       ),
+//     );
+//   }
 
+//   void _showLoginDialog(BuildContext context) {
+//     TextEditingController emailController = TextEditingController();
+//     TextEditingController passwordController = TextEditingController();
+
+//     showDialog(
+//       context: context,
+//       builder: (BuildContext context) {
+//         return AlertDialog(
+//           content: Column(
+//             mainAxisSize: MainAxisSize.min,
+//             children: [
+//               TextField(
+//                 controller: emailController,
+//                 decoration: InputDecoration(labelText: 'Email'),
+//               ),
+//               TextField(
+//                 controller: passwordController,
+//                 decoration: InputDecoration(labelText: 'Hasło'),
+//                 obscureText: true,
+//               ),
+//             ],
+//           ),
+//           actions: [
+//             TextButton(
+//               onPressed: () {
+//                 _login(emailController.text, passwordController.text);
+//                 Navigator.pop(context);
+//               },
+//               child: Text('Zaloguj'),
+//             )
+//           ],
+//         );
+//       },
+//     );
+//   }
+
+//   Future<void> _login(String email, String password) async {
+//     try {
+//       UserCredential userCredential = await _auth.signInWithEmailAndPassword(
+//         email: email,
+//         password: password,
+//       );
+//       setState(() {
+//         _user = userCredential.user;
+//       });
+//       Fluttertoast.showToast(
+//         msg: "Zalogowano",
+//         toastLength: Toast.LENGTH_SHORT,
+//         gravity: ToastGravity.BOTTOM,
+//       );
+//     } catch (e) {
+//       Fluttertoast.showToast(
+//         msg: "Nieprawidłowy email lub hasło",
+//         toastLength: Toast.LENGTH_SHORT,
+//         gravity: ToastGravity.BOTTOM,
+//       );
+//     }
+//   }
+
+//   void _showRegistrationDialog(BuildContext context) {
+//     TextEditingController emailController = TextEditingController();
+//     TextEditingController passwordController = TextEditingController();
+
+//     showDialog(
+//       context: context,
+//       builder: (BuildContext context) {
+//         return AlertDialog(
+//           content: Column(
+//             mainAxisSize: MainAxisSize.min,
+//             children: [
+//               TextField(
+//                 controller: emailController,
+//                 decoration: InputDecoration(labelText: 'Email'),
+//               ),
+//               TextField(
+//                 controller: passwordController,
+//                 decoration: InputDecoration(labelText: 'Hasło'),
+//                 obscureText: true,
+//               ),
+//             ],
+//           ),
+//           actions: [
+//             TextButton(
+//               onPressed: () {
+//                 _register(emailController.text, passwordController.text);
+//                 Navigator.pop(context);
+//               },
+//               child: Text('Zarejestruj'),
+//             )
+//           ],
+//         );
+//       },
+//     );
+//   }
+
+//   Future<void> _register(String email, String password) async {
+//     try {
+//       UserCredential userCredential =
+//           await _auth.createUserWithEmailAndPassword(
+//         email: email,
+//         password: password,
+//       );
+//       setState(() {
+//         _user = userCredential.user;
+//       });
+//       Fluttertoast.showToast(
+//         msg: "Zarejestrowano",
+//         toastLength: Toast.LENGTH_SHORT,
+//         gravity: ToastGravity.BOTTOM,
+//       );
+//     } catch (e) {
+//       Fluttertoast.showToast(
+//         msg: "Email jest już zajęty lub hasło jest za krótkie",
+//         toastLength: Toast.LENGTH_SHORT,
+//         gravity: ToastGravity.BOTTOM,
+//       );
+//     }
+//   }
+
+//   Future<void> _logout() async {
+//     await _auth.signOut();
+//     setState(() {
+//       _user = null;
+//     });
+//     Fluttertoast.showToast(
+//       msg: "Wylogowano",
+//       toastLength: Toast.LENGTH_SHORT,
+//       gravity: ToastGravity.BOTTOM,
+//     );
+//   }
+// }
