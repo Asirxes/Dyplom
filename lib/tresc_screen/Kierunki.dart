3128c986f3c5c00cbdd53c7ffc7b622d2874998a
Monika4270 <monika42701@gmail.com>
2023-11-16 00:02:06 +0100

    Update Kierunki.dart

--- lib/tresc_screen/Kierunki.dart
+++ lib/tresc_screen/Kierunki.dart
@@ -4,7 +4,7 @@ import 'package:dyplom/ranking_screen/RatingModel.dart';
 import 'package:dyplom/tresc_screen/Category.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_rating_bar/flutter_rating_bar.dart';
-import 'package:firebase_core/firebase_core.dart';
+//import 'package:firebase_core/firebase_core.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
